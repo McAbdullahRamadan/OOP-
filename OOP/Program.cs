995e669ec3b5c221baa9c91e0c9af2f94a5f0361
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2025-12-15 18:25:04 +0200

    OverLoadd

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,5 +1,4 @@
-﻿
-//pass By Value Example
+﻿//pass By Value Example
 //string firstnam, lastname;
 //firstnam = "John";
 //lastname = "Doe";
@@ -17,18 +16,67 @@
 //Console.ReadLine();
 
 //pass By Ref Example
-string firstnam, lastname;
-firstnam = "John";
-lastname = "Doe";
-Console.WriteLine($"Inside Main Method Before Calling {firstnam},{lastname}");
+//string firstnam, lastname;
+//firstnam = "John";
+//lastname = "Doe";
+//Console.WriteLine($"Inside Main Method Before Calling {firstnam},{lastname}");
 
-ChangeValues(ref firstnam, ref lastname);
-Console.WriteLine($"Inside Main Method After Calling {firstnam},{lastname}");
+//ChangeValues(ref firstnam, ref lastname);
+//Console.WriteLine($"Inside Main Method After Calling {firstnam},{lastname}");
 
-static void ChangeValues(ref string firstnam, ref string lastname)
+//static void ChangeValues(ref string firstnam, ref string lastname)
+//{
+//    firstnam = "Dark";
+//    lastname = "Smith";
+//    Console.WriteLine($"Inside ChangeValues Method: {firstnam},{lastname}");
+//}
+class Program
 {
-    firstnam = "Dark";
-    lastname = "Smith";
-    Console.WriteLine($"Inside ChangeValues Method: {firstnam},{lastname}");
+
+    static void Main(string[] arg)
+    {
+
+        string guestName = "";
+        Console.WriteLine(addNamber(5, 10));
+        Console.WriteLine(addNamber(5, 10, 15));
+        Console.WriteLine("dear guest : waht is your name ");
+        guestName = Console.ReadLine();
+        if (guestName == string.Empty)
+        {
+            WlecomeGuest();
+        }
+        else
+        {
+            WlecomeGuest(guestName);
+        }
+
+        Console.ReadLine();
+    }
+    public static void WlecomeGuest()
+    {
+
+        Console.WriteLine($"Can i help you ");
+
+    }
+    public static void WlecomeGuest(string name)
+    {
+
+        Console.WriteLine($"Good day {name} Welcome to my app ");
+
+    }
+    public static int addNamber(int x, int y)
+    {
+
+        return x + y;
+    }
+    public static int addNamber(int x, int y, int r)
+    {
+
+        return x + y + r;
+    }
+
+
 }
-Console.ReadLine();
+
+
+
