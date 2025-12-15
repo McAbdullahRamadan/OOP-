4a037deecb8e1aa0b8da78527dbf6069e9703ad1
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2025-12-15 17:04:29 +0200

    pass By Value Example

---
+++ OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//pass By Value Example
+string firstnam, lastname;
+firstnam = "John";
+lastname = "Doe";
+Console.WriteLine($"Inside Main Method Before Calling {firstnam},{lastname}");
+
+ChangeValues(firstnam, lastname);
+Console.WriteLine($"Inside Main Method After Calling {firstnam},{lastname}");
+
+static void ChangeValues(string firstnam, string lastname)
+{
+    firstnam = "Dark";
+    lastname = "Smith";
+    Console.WriteLine($"Inside ChangeValues Method: {firstnam},{lastname}");
+}
+Console.ReadLine();
