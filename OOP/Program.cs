470e10d0446475752b844f02c7105fa7df763c0a
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2025-12-15 17:06:48 +0200

    Pass By Reference

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,14 +1,31 @@
 ﻿
 //pass By Value Example
+//string firstnam, lastname;
+//firstnam = "John";
+//lastname = "Doe";
+//Console.WriteLine($"Inside Main Method Before Calling {firstnam},{lastname}");
+
+//ChangeValues(firstnam, lastname);
+//Console.WriteLine($"Inside Main Method After Calling {firstnam},{lastname}");
+
+//static void ChangeValues(string firstnam, string lastname)
+//{
+//    firstnam = "Dark";
+//    lastname = "Smith";
+//    Console.WriteLine($"Inside ChangeValues Method: {firstnam},{lastname}");
+//}
+//Console.ReadLine();
+
+//pass By Ref Example
 string firstnam, lastname;
 firstnam = "John";
 lastname = "Doe";
 Console.WriteLine($"Inside Main Method Before Calling {firstnam},{lastname}");
 
-ChangeValues(firstnam, lastname);
+ChangeValues(ref firstnam, ref lastname);
 Console.WriteLine($"Inside Main Method After Calling {firstnam},{lastname}");
 
-static void ChangeValues(string firstnam, string lastname)
+static void ChangeValues(ref string firstnam, ref string lastname)
 {
     firstnam = "Dark";
     lastname = "Smith";
