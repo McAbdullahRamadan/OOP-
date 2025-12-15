983a2f59ab38a8b39b7d3ea202957be34f52c14d
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2025-12-15 19:24:38 +0200

    Classes and Objects

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -30,53 +30,118 @@
 //    lastname = "Smith";
 //    Console.WriteLine($"Inside ChangeValues Method: {firstnam},{lastname}");
 //}
-class Program
+BankAccount bankAccount = new BankAccount();
+bankAccount.BankName = "ABC Bank";
+bankAccount.BranchName = "Main Branch";
+bankAccount.BranchAdress = "123 Main St, Cityville";
+bankAccount.AccountName = "John Doe";
+bankAccount.AccountCurrency = "USD";
+bankAccount.AccountNumber = "123456789";
+bankAccount.Balance = 1000;
+Console.WriteLine(bankAccount.AccountName);
+Console.WriteLine(bankAccount.Balance);
+bankAccount.Withdraw(200);
+bankAccount.Deposit(500);
+
+
+class BankAccount
 {
+    public string BankName { get; set; }
+    public string BranchName { get; set; }
+    public string BranchAdress { get; set; }
+    public string AccountName { get; set; }
+    public string AccountCurrency { get; set; }
 
-    static void Main(string[] arg)
+    public string AccountNumber { get; set; }
+
+    public decimal Balance { get; set; }
+    public void Withdraw(decimal amount)
     {
-
-        string guestName = "";
-        Console.WriteLine(addNamber(5, 10));
-        Console.WriteLine(addNamber(5, 10, 15));
-        Console.WriteLine("dear guest : waht is your name ");
-        guestName = Console.ReadLine();
-        if (guestName == string.Empty)
+        decimal newBalance = Balance - amount;
+        if (newBalance < 0)
         {
-            WlecomeGuest();
+            Console.WriteLine("Insufficient funds for this withdrawal.");
         }
         else
         {
-            WlecomeGuest(guestName);
+
+            Console.WriteLine($"Withdrew {amount:C}");
+            Console.WriteLine($"your new Balance for {AccountNumber} is {newBalance} ");
         }
-
-        Console.ReadLine();
+        Console.WriteLine(DateTime.Now);
     }
-    public static void WlecomeGuest()
+
+    public void Deposit(decimal amount)
     {
+        decimal newBalance = Balance + amount;
+        if (newBalance < 0)
+        {
+            Console.WriteLine("Insufficient funds for this withdrawal.");
+        }
+        else
+        {
 
-        Console.WriteLine($"Can i help you ");
-
+            Console.WriteLine($"Withdrew {amount:C}");
+            Console.WriteLine($"your new Balance for {AccountNumber} is {newBalance} ");
+        }
+        Console.WriteLine(DateTime.Now);
     }
-    public static void WlecomeGuest(string name)
-    {
-
-        Console.WriteLine($"Good day {name} Welcome to my app ");
-
-    }
-    public static int addNamber(int x, int y)
-    {
-
-        return x + y;
-    }
-    public static int addNamber(int x, int y, int r)
-    {
-
-        return x + y + r;
-    }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//static void Main(string[] arg)
+//{
+
+//    string guestName = "";
+//    Console.WriteLine(addNamber(5, 10));
+//    Console.WriteLine(addNamber(5, 10, 15));
+//    Console.WriteLine("dear guest : waht is your name ");
+//    guestName = Console.ReadLine();
+//    if (guestName == string.Empty)
+//    {
+//        WlecomeGuest();
+//    }
+//    else
+//    {
+//        WlecomeGuest(guestName);
+//    }
+
+//    Console.ReadLine();
+//}
+//public static void WlecomeGuest()
+//{
+
+//    Console.WriteLine($"Can i help you ");
+
+//}
+//public static void WlecomeGuest(string name)
+//{
+
+//    Console.WriteLine($"Good day {name} Welcome to my app ");
+
+//}
+//public static int addNamber(int x, int y)
+//{
+
+//    return x + y;
+//}
+//public static int addNamber(int x, int y, int r)
+//{
+
+//    return x + y + r;
+//}
+
+
 
 
 
